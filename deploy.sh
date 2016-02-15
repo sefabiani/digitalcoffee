94905a474fe70cbeda91957da8ff6e4a2ce05fa4
sefabiani <sebastien.fabiani@digitalcoffee.fr>
2016-02-16 00:37:35 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 echo -n "Deployment.."
 
 deployment=$(curl -X POST -H "Accept: application/vnd.com.sensiolabs.insight+xml" "https://368761df-60fe-41ae-ae48-5788ac6c060e:2dcfb791ae2186c57b2cabdc5dd6c383dfd2dfd3a736a539ac8e2502495dc738@insight.sensiolabs.com/api/projects/4f1e5660-82c3-4db5-950d-d30cf76d876c/analyses")
@@ -11,11 +10,11 @@ while [ $check = 0 ];do
 echo -n "."
 sleep 2
 
-if $(echo $deployment | grep -q '<failed>false</failed>') 
+if [ $(echo $deployment | grep -c '<failed>false</failed>') -ne 0 ]
 then
 	echo " successful !"
 	exit 0
-elif $(echo $deployment | grep -q '<failed>true</failed>')
+elif [ $(echo $deployment | grep -c '<failed>true</failed>') -ne 0 ]
 then
 	echo " failed !"
 	exit 1
