abc517fac0e041a31a1516d8db2c8d49b94e6759
sefabiani <sebastien.fabiani@digitalcoffee.fr>
2016-02-16 10:16:00 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 echo -n "Deployment.."
-curl -X POST -H "Accept: application/vnd.com.sensiolabs.insight+xml" "https://368761df-60fe-41ae-ae48-5788ac6c060e:2dcfb791ae2186c57b2cabdc5dd6c383dfd2dfd3a736a539ac8e2502495dc738@insight.sensiolabs.com/api/projects/4f1e5660-82c3-4db5-950d-d30cf76d876c/analyses"
+#curl -X POST -H "Accept: application/vnd.com.sensiolabs.insight+xml" "https://368761df-60fe-41ae-ae48-5788ac6c060e:2dcfb791ae2186c57b2cabdc5dd6c383dfd2dfd3a736a539ac8e2502495dc738@insight.sensiolabs.com/api/projects/4f1e5660-82c3-4db5-950d-d30cf76d876c/analyses"
+until deployment $(curl --output /dev/null --silent --head --fail -X POST -H "Accept: application/vnd.com.sensiolabs.insight+xml" "https://368761df-60fe-41ae-ae48-5788ac6c060e:2dcfb791ae2186c57b2cabdc5dd6c383dfd2dfd3a736a539ac8e2502495dc738@insight.sensiolabs.com/api/projects/4f1e5660-82c3-4db5-950d-d30cf76d876c/analyses"); do
+  printf '.'
+  sleep 3
+  
+  if [ $(echo $deployment | grep -c '<failed>false</failed>') -ne 0 ]
+  then
+    echo " successful !"
+    exit 0
+  elif [ $(echo $deployment | grep -c '<failed>true</failed>') -ne 0 ]
+  then
+    echo " failed !"
+    exit 1
+  fi
+done
 #deployment=$(curl -X POST -H "Accept: application/vnd.com.sensiolabs.insight+xml" "https://368761df-60fe-41ae-ae48-5788ac6c060e:2dcfb791ae2186c57b2cabdc5dd6c383dfd2dfd3a736a539ac8e2502495dc738@insight.sensiolabs.com/api/projects/4f1e5660-82c3-4db5-950d-d30cf76d876c/analyses")
 
 #check=0
