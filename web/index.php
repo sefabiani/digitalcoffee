0861a96f37fd70d606b52819c872fbddabb4401a
sebfabiani <seb.fabiani@gmail.com>
2016-02-21 01:49:30 +0100

    favicon and robots.txt

--- web/index.php
+++ web/index.php
@@ -4,8 +4,16 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app->get('/', function () {
+    return 'Hello world !';
+});
+
 $app->get('/hello', function () {
-    return 'Hello!';
+    return 'Hello !';
+});
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello ' . $app->escape($name);
 });
 
 $app->run();
